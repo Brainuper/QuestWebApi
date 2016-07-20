c50c6412b6126fe798f385da9b2772ce47d713e2
iGilga <pyua87@gmail.com>
2016-07-21 01:01:50 +0300

    Added webpack for test use jasmine.

---
+++ webpack/test-webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: {
+    unit: './test/index'
+  },
+  output: {
+    path: 'tmp',
+    filename: 'backend.spec.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }],
+  },
+  plugins: [
+    new webpack.BannerPlugin('require("source-map-support").install();', {
+      raw: true,
+      entryOnly: false
+    })
+  ],
+  devTool: '#eval-source-map',
+  debug: true,
+  target: 'node',
+  externals: [nodeExternals()]
+};

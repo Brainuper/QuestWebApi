599c4b79e3373cea61cfa8748e2506c6cad0502f
iGilga <pyua87@gmail.com>
2016-07-22 21:57:55 +0300

    Added subject api in routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import * as Controller from './components';
+
 export default function setup(app) {
   app.get('/api/version', (req, res) => {
     res.status(200);
     res.json({version: "1.0"});
-  })
+  });
+
+  app.use('/api/subjects', Controller.subjectController());
 };

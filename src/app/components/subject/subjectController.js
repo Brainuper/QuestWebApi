c0716ff76861ff29261f719118192608d011ef2d
iGilga <pyua87@gmail.com>
2016-07-29 04:10:19 +0300

    Refactoring controller subject

--- src/app/components/subject/subjectController.js
+++ src/app/components/subject/subjectController.js
@@ -1,31 +1,38 @@
-import {Router} from 'express';
 import SubjectService from './subjectService';
 
-export default function() {
-  var subjectController = Router();
-  var subjectService = new SubjectService();
-  subjectController.route('/:id')
-    .get((req, res) => {
-      let subject = subjectService.getById(req.params.id);
+export default class SubjectController {
+  constructor() {
+    this.subjectService = new SubjectService();
+  }
+
+  getAll() {
+    return (req, res) => {
+      let subjects = this.subjectService.getAll();
+      res.json(subjects);
+    }
+  }
+
+  getById() {
+    return (req, res) => {
+      let subject = this.subjectService.getById(req.params.id);
       res.json(subject);
-    })
-    .put((req, res) => {
+    }
+  }
+
+  add() {
+    return (req, res) => {
+      let createSubject = req.body;
+      createSubject = this.subjectService.add(createSubject);
+      res.status(201).json(createSubject);
+    }
+  }
+
+  edit() {
+    return (req, res) => {
       let editSubject = req.body;
       editSubject.id = req.params.id;
-      editSubject = subjectService.update(editSubject);
+      editSubject = this.subjectService.update(editSubject);
       res.json(editSubject);
-    });
-
-  subjectController.route('/')
-    .get((req, res) => {
-      let subjects = subjectService.getAll();
-      res.json(subjects);
-    })
-    .post((req, res) => {
-      let createSubject = req.body;
-      createSubject = subjectService.add(createSubject);
-      res.status(201).json(createSubject);
-    });
-
-  return subjectController;
+    }
+  }
 };

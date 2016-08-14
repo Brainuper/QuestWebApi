37d988e5a2f8611b074d0f246a68aa96430ccf75
iGilga <pyua87@gmail.com>
2016-08-14 05:10:30 +0300

    Implemented methods

--- src/app/components/quest/questService.js
+++ src/app/components/quest/questService.js
@@ -1,13 +1,41 @@
+import {Quest, Subject} from '../../models';
+
 export default class QuestService {
   constructor() {
-
+    this.Model = Quest;
   }
 
-  getAll() {}
+  getAll() {
+    return this.Model.findAll({
+      include: [Subject]
+    });
+  }
 
-  getById(id) {}
+  getById(id) {
+    return this.Model.findById(id);
+  }
 
-  add(subject) {}
+  add(quest) {
+    // return this.Model.sync().then(() => {
+      return this.Model.create(quest, {
+        include: [Subject]
+      });
+    // })
+  }
 
-  update(subject) {}
+  update(id, quest) {
+    return this.Model.update(quest, {
+      where: {
+        id: id
+      }
+    });
+  }
+
+  remove(id) {
+    return this.Model.destroy({
+      where: {
+        id: id
+      }
+    });
+  }
 };

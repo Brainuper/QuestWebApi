6e21113c68269842b053481159815c0329b12886
iGilga <pyua87@gmail.com>
2016-08-14 05:06:40 +0300

    Added model quest

---
+++ src/app/models/quest.js
@@ -0,0 +1,16 @@
+import Sequelize from 'sequelize';
+import sequelize from '../db';
+
+var Quest = sequelize.define('quest', {
+  text: {
+    type: Sequelize.TEXT
+  },
+  option: {
+    type: Sequelize.ARRAY(Sequelize.TEXT)
+  },
+  answer: {
+    type: Sequelize.INTEGER
+  }
+});
+
+export default Quest;

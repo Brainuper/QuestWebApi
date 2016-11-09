f65aaa9d254263819111f361e25c8ac1e0f75f2c
iGilga <pyua87@gmail.com>
2016-11-09 04:51:30 +0300

    remodeling quest

--- src/app/models/quest.js
+++ src/app/models/quest.js
@@ -1,16 +1,44 @@
-import Sequelize from 'sequelize';
-import sequelize from 'db';
+import mongoose, {Schema} from 'mongoose';
 
-var Quest = sequelize.define('quest', {
-  text: {
-    type: Sequelize.TEXT
-  },
-  option: {
-    type: Sequelize.ARRAY(Sequelize.TEXT)
-  },
-  answer: {
-    type: Sequelize.INTEGER
+const {
+  Types: {
+    ObjectId,
+    Mixed
   }
+} = Schema;
+
+const model = {
+  id: ObjectId,
+  body: Mixed,
+  option: [String],
+  answer: String,
+  createAt: {
+    type: Date,
+    default: Date.now
+  },
+  updateAt: {
+    type: Date,
+    default: Date.now
+  },
+  subjects: [
+    {
+      type: ObjectId,
+      ref: 'Subject'
+    }
+  ]
+};
+
+const QuestSchema = new Schema(model);
+
+QuestSchema.pre('save', function(next) {
+  let self = this;
+  let date = Date.now();
+  if (self.updateAt < date) {
+    self.updateAt = date;
+  }
+  return next();
 });
 
-export default Quest;
+const Quest = mongoose.model('Quest', QuestSchema);
+
+export {Quest, QuestSchema};

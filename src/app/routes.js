efa44802ac46139140119e9f39410b66d4a47d64
iGilga <pyua87@gmail.com>
2016-11-16 02:34:07 +0300

    Remove comment and include route to randomazer

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,24 +1,12 @@
 import * as component from './components';
-import db from 'db';
-import logger from 'logger';
+import 'db';
+// import logger from 'logger';
 
 export default function setup(app) {
   app.get('/api/version', (req, res) => {
     res.status(200);
     res.json({version: '1.0'});
   });
-
-  // app.use((req, res, next) => {
-  //   db
-  //     .sync()
-  //     .then(() => {
-  //       next();
-  //     })
-  //     .catch((err) => {
-  //       logger.error(err);
-  //       res.status(500).json(err.message);
-  //     });
-  // });
 
   app.use('/api/subjects', component.subjectRouter());
   app.use('/api/quests', component.questRouter());
